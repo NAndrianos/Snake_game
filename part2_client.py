85a5266a4ca4980684c1c703db05f827f6c640c4
ntandrianos <ntandrianos@gmail.com>
2023-03-29 21:57:25 -0700

    Implemented part2 client and server

--- part2_client.py
+++ part2_client.py
@@ -1,2 +1,58 @@
+# Group#: G37
+# Student Names: Ryley McRae & Niko Andrianos
+
+"""
+    This program implements a client for a UDP Pinger 
+    application on a local host.
+"""
+
+from socket import *
+import time
+
 if __name__ == "__main__":
-    print("Hello world")
+    # Pre-define a list of messages to ping the server with
+    messageList = [
+        "PING 1 - hello world!",
+        "PING 2 - what is your name?",
+        "PING 3 - SPAM12345",
+        "PING 4 - 99999900009999999",
+        "PING 5 - ______^^^^^__===------         ___"
+    ]
+
+    # Create variables to idenfity the server
+    serverName = "localhost"
+    serverPortParams = ("127.0.0.1", 12000)
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+    # Set a timeout for 1 second while client waits for serve
+    clientSocket.settimeout(1.0)
+
+    # Ping the server once with each message
+    for message in messageList:
+        
+        # Start RTT time tracker
+        messageTimerStart = time.time()
+
+        # Send message over port
+        clientSocket.sendto(message.encode(), serverPortParams)
+
+        # Use a try/except to handle a timeout from the server side
+        try:
+            # Read reply
+            modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+        
+            # End RTT time tracker
+            messageTimerEnd = time.time()
+
+            # Calculate the RTT
+            roundTripTime = messageTimerEnd - messageTimerStart
+
+            # Print reply and RTT for the message
+            print(f"The recieved message: {modifiedMessage.decode()}\nThe RTT for the message: {roundTripTime} Seconds")
+
+        except timeout:
+            # If no reply is avaliable print the timeout message
+            print("request timed out")
+
+    # Close client after each message is sent
+    clientSocket.close()
--- part2_server.py
+++ part2_server.py
@@ -1,2 +1,43 @@
+# Group#: G37
+# Student Names: Ryley McRae & Niko Andrianos
+
+"""
+    This program implements a server for a UDP Pinger 
+    application on a local host.
+"""
+
+from socket import *
+import random
+import time
+
 if __name__ == "__main__":
-    print("Hello world")
+    # Create variables to set up server
+    serverPort = 12000
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+
+    # Bind to specified port
+    serverSocket.bind(("127.0.0.1", serverPort))
+
+    print("The server is ready to recieve!")
+
+    # Unlessed process is killed, let the server listen
+    while True:
+        message, clientAddress = serverSocket.recvfrom(2048)
+
+        # Create a flag with a 10% probability to simulate packet loss
+        ignoreMessageFlag = True if random.random() < 0.1 else False
+
+        # Skip this iteration: 'drop the packet'
+        if ignoreMessageFlag is True:
+            continue
+        
+        # Perform string manipulation to the desired format
+        decodedMessageList = message.decode().split(" ")
+        responseMessage = f"{decodedMessageList[0]} {decodedMessageList[1]} - ditto"
+
+        # Generate a random sleep time between 5 and 50 milliseconds
+        sleepTime = random.uniform(0.005, 0.05)
+        time.sleep(sleepTime)
+
+        # Reply to the client with response message
+        serverSocket.sendto(responseMessage.encode(), clientAddress)
